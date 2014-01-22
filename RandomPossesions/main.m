00813f1848909def85e58183c27dfe2b352923a1
Steve Chakif <schakif@Steves-MacBook-Pro.local>
2014-01-22 14:40:49 -0500

    Initial Commit

---
+++ RandomPossesions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossesions
+//
+//  Created by Steve Chakif on 1/22/14.
+//  Copyright (c) 2014 Steve Chakif. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
